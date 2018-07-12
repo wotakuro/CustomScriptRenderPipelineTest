6ed174f69e3adb02bdf7b25c79689dd025bf2e7d
wotakuro <wotakuro@gmail.com>
2018-07-12 16:29:46 +0900

    Auto Upgrade SpiteAtlas Script(2018.2)

---
+++ Assets/Editor/SpriteAlasFix.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.U2D;
+using UnityEngine.U2D;
+
+#if UNITY_2018_2_OR_NEWER
+[InitializeOnLoad]
+public class SpriteAlasFix  {
+    static SpriteAlasFix()
+    {
+        Execute("Assets/Sprites/Misaki.spriteatlas");
+        Execute("Assets/Sprites/Yuko.spriteatlas");
+    }
+    private static void Execute(string path)
+    {
+        var data = AssetDatabase.LoadAssetAtPath(path,typeof( SpriteAtlas) );
+
+        SerializedObject serializedObject = new UnityEditor.SerializedObject(data);
+        var packing = serializedObject.FindProperty("m_EditorData.packingSettings.enableTightPacking");
+        var rotating = serializedObject.FindProperty("m_EditorData.packingSettings.enableRotation");
+
+        bool originPacking = packing.boolValue;
+        bool originRotating = rotating.boolValue;
+        packing.boolValue = false;
+        rotating.boolValue = false;
+        serializedObject.ApplyModifiedProperties();
+        if( originPacking || originRotating)
+        {
+            AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+        }
+    }
+}
+#endif
